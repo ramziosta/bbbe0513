f38fd0562a460226472beb652a009c6f2ca267c6
Ramzi Osta <76601606+ramziosta@users.noreply.github.com>
2022-05-15 16:30:02 -0400

    user permission n roles work, router and auth works 100

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,67 @@
-import NavBar from "./components/navbar.js";
-import react, { useState } from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import AllData from "./frontend/alldata.js";
 import CreateAccount from "./frontend/createaccount.js";
+import DashBoard from "./frontend/DashBoard";
 import Deposit from "./frontend/deposit.js";
+import Footer from "./components/footer";
 import Home from "./frontend/home.js";
-import Withdraw from "./frontend/withdraw.js";
 import Login from "./frontend/login.js";
 import Logout from "./frontend/logout";
-import Footer from "./components/footer";
-import DashBoard from "./frontend/DashBoard";
-import Cookies from "js-cookie";
+import NavBar from "./components/navbar.js";
+import Withdraw from "./frontend/withdraw.js";
+import Layout from "./components/Layout";
+import Editor from "./components/Editor";
+import Admin from "./components/Admin";
+import Missing from "./components/Missing";
+import Unauthorized from "./components/Unauthorized";
+import Lounge from "./components/Lounge";
+import LinkPage from "./components/LinkPage";
+import RequireAuth from "./components/RequireAuth";
+import ROLES from "./config/roles_list";
 import "./styles/Home.css";
 
-
-let loggedInUser = Cookies.get("loggedInUser");
-
 function App() {
-  const [loginData, setLoginData] = useState(
-    Cookies.get("loginData") ? JSON.parse(Cookies.get("loginData")) : null
-  );
-  let user;
-  //! change name to user..
-  !loginData ? (user = Cookies.get("name")) : (user = loginData.user);
   return (
     <>
-      <BrowserRouter>
-        <NavBar loggedInUser={loggedInUser} />
-        <div className="container" style={{ padding: "10px" }}></div>
-        {loggedInUser || loginData ? (
-          <div
-            style={{
-              position: "relative",
-              color: "red",
-              fontSize: "1.2rem",
-              textAlign: "right",
-              padding: "15px",
-            }}
-          >
-            {`Welcome, ${user || ""} `}
-          </div>
-        ) : (
-          ""
-        )}
-
-        <Routes>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* public routes */}
           <Route path="/" exact element={<Home />} />
           <Route path="/CreateAccount/" element={<CreateAccount />} />
-          <Route path="/dashboard/" exact element={<DashBoard />} />
-          <Route path="/deposit/" exact element={<Deposit />} />
-          <Route path="/withdraw/" exact element={<Withdraw />} />
-          <Route path="/alldata/" exact element={<AllData />} />
           <Route path="/login/" exact element={<Login />} />
           <Route path="/logout/" exact element={<Logout />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+          <Route path="linkpage" element={<LinkPage />} />
+          <Route path="unauthorized" element={<Unauthorized />} />
+
+          {/* protected routes with role permission */}
+          <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+            <Route path="/dashboard/" exact element={<DashBoard />} />
+            <Route path="/deposit/" exact element={<Deposit />} />
+            <Route path="/withdraw/" exact element={<Withdraw />} />
+          </Route>
+
+          <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
+            <Route path="editor" element={<Editor />} />
+          </Route>
+
+          <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+            <Route path="admin" element={<Admin />} />
+            <Route path="/alldata/" exact element={<AllData />} />
+          </Route>
+
+          <Route
+            element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}
+          >
+            <Route path="lounge" element={<Lounge />} />
+          </Route>
+
+          {/* catch all 404 */}
+          <Route path="*" element={<Missing />} />
+        </Route>
+      </Routes>
+      <Footer />
     </>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,42 +1,26 @@
-import "../src/styles/index.scss";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UserContext } from "./components/context.js";
-import { AuthProvider } from './context/AuthProvider';
-import "../src/styles/index.css";
+import { AuthProvider } from "./context/AuthProvider";
 import App from "./App";
+import "../src/styles/index.css";
+import "../src/styles/index.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 import reportWebVitals from "./reportWebVitals";
-//> setup userContext here to provide the information to the whole app
 
 ReactDOM.render(
   <React.StrictMode>
-   <AuthProvider>
-      <UserContext.Provider
-          value={{
-            users: [
-              {
-                user: "",
-                email: "",
-                pwd: "",
-                balance:null,
-                created:"",
-                transactionType: "",
-                transactionAmount: null,
-                transactionDate: null,
-              },
-            ],
-            transactions:[],
-            sessionActivity:[],
-            log: false,
-            login: false,
-            register: false,
-          }}
-        >
-    <App />
-    </UserContext.Provider>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <UserContext.Provider
+          value={{ users: [{}], transactions: [], sessionActivity: [] }}>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </UserContext.Provider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
